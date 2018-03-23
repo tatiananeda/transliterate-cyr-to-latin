e72a6cc910cde383f8b5487da003cc0fd75400eb
nedavniat <nedavniat@gmail.com>
2018-03-23 17:24:03 +0300

    transliterate

---
+++ module/README.md
@@ -0,0 +1,18 @@
+#Module for transliteration of cyrillic into latin under the rules of [ISO 9](https://en.wikipedia.org/wiki/ISO_9)
+
+###Enable
+to use at server side run:
+```
+nmp install transliterate-cyr-to-latin
+```
+
+to use in browser download this module refer to the file module/index.js using the src attribute in the script tag.
+
+
+###Usage
+The module exposes *transliterate* function which should be called with one parameter of the type String. The function returns an output of the type String with all cyrillic characters replaced
+with latin characters.
+```javascript
+transliterate('Строка на русском языке або текст українською мовою');
+```
+a method *.noConflict* is available on *transliterate* object. This method, when invoken, restores the namespace transliterate to the previous value, if any.
---
+++ module/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+(function(){
+  const root = this;
+  const prev_module = root.transliterate;
+
+  const library = {
+    'A': 'A',
+    'а': 'a',
+    'Б': 'B',
+    'б': 'b',
+    'В':'V',
+    'в':'v',
+    'Г':'G',
+    'г':'g',
+    'Ґ':'Ģ',
+    'ґ':'ģ',
+    'Д':'D',
+    'д':'d',
+    'Е':'E',
+    'е':'e',
+    'Ё':'Yo',
+    'ё':'yo',
+    'Є':'È',
+    'є':'ê',
+    'Ж':'Ž',
+    'ж':'ž',
+    'З':'Z',
+    'з':'z',
+    'И':'I',
+    'и':'i',
+    'І':'Ì',
+    'і':'ì',
+    'Ї':'Ï',
+    'ї':'ï',
+    'Й':'J',
+    'й':'j',
+    'К':'K',
+    'к':'k',
+    'Л':'L',
+    'л':'l',
+    'М':'M',
+    'м':'m',
+    'Н':'N',
+    'н':'n',
+    'О':'O',
+    'о':'o',
+    'П':'P',
+    'п':'p',
+    'Р':'R',
+    'р':'r',
+    'С':'S',
+    'с':'s',
+    'Т':'T',
+    'т':'t',
+    'У':'U',
+    'у':'u',
+    'Ф':'F',
+    'ф':'f',
+    'Х':'H',
+    'х':'h',
+    'Ц':'C',
+    'ц':'c',
+    'Ч':'Č',
+    'ч':'č',
+    'Ш':'Ŝ',
+    'ш':'ŝ',
+    'Щ':'Ŝ',
+    'щ':'ŝ',
+    'Ъ':'',
+    'ъ':'',
+    'Ы':'Y',
+    'ы':'y',
+    'Ь':'',
+    'ь':'',
+    'Ю':'Û',
+    'ю':'û',
+    'Я':'Â',
+    'я':'â',
+    '’':''
+  };
+
+  const transliterate = (string) => {
+    string = string.replace(/зг/gi, 'zgh');
+    let result = '';
+    for(let i = 0; i < string.length; i++){
+      result += library[string[i]] || library[string[i]] === '' ? library[string[i]] : string[i];
+    }
+    return result;
+  };
+
+  transliterate.noConflict = function(){
+    root.transliterate = prev_module;
+  };
+
+  if( typeof exports !== 'undefined' ) {
+    if( typeof module !== 'undefined' && module.exports ) {
+      exports = module.exports = transliterate;
+    }
+    exports.transliterate = transliterate;
+  }
+  else {
+    root.transliterate = transliterate;
+  }
+
+}).call(this);
